a29e33b79b4aa0d1be8f47f2345fbc1837e35be2
KD0NKS <andrew.m.fairhurst@gmail.com>
2014-05-20 19:03:15 -0500

    Inheritance model of the data connections is working.  Scope of "this" still broken.

--- README.md
+++ README.md
@@ -2,3 +2,18 @@ APRSViewJS
 ==========
 
 Experimental APRS Client using NodeWebkit
+
+Running From Source:
+
+Local Dev Setup
+===============
+Install Node.js (of course)
+Install Yeoman globally: npm install –g yeoman
+Install the Yeoman Node WebKit Generator globally: npm install –g generator-node-webkit
+Install Node WebKit globally: npm install –g nodewebkit
+Install the Grunt CLI globally: npm install –g grunt-cli
+
+Run the project
+===============
+Navigate to the project directory
+> nodewebkit app
--- app/js/techpire/APRSConnectionManager.js
+++ app/js/techpire/APRSConnectionManager.js
@@ -1,11 +1,44 @@
 var TechpireAPRS = require('TechpireAPRS')
 		;
 
+//var SOFTWARE_NAME = 'testsoftware';
+//var SOFTWARE_VERSION = 0;
+		
 function APRSConnectionManager() {
 	var self = this;
 	
 	var connectionFactory = new TechpireAPRS.APRSDataConnectionFactory();
+	var dataConnection = null;
 	
 	// TODO: foreach data connection
-	connectionFactory.CreateDataConnection('');
+	var args = Array();
+	args['connectionType'] = 'APRSIS';
+	args['callsign'] = 'N0CALL';
+	args['passcode'] = '-1';
+	args['host'] = '';
+	args['port'] = 10154;
+	
+	args['isEnabled'] = false;
+	args['isReconnectOnFailure'] = true;
+	
+	args['softwareName'] = 'testsoftware';
+	args['softwareVersion'] = 0;
+	
+	dataConnection = connectionFactory.CreateDataConnection(args);
+	
+	console.log(dataConnection.host);
+	console.log(dataConnection.callsign);
+	console.log(dataConnection);
+	
+	dataConnection.Read();
+	
+	//dataConnection.on('data', function(data) {
+	//	console.log(data);
+	//});
+	
+	try {
+		dataConnection.Connect();
+	} catch(e) {
+		console.log(e);
+	}
 }
